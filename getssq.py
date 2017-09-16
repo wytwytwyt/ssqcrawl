f01f16bf6e93a5749004a4417a142a51576e078a
wyt <353981086@qq.com>
2017-09-16 19:50:33 +0800

    update pagenums

--- getssq.py
+++ getssq.py
@@ -53,7 +53,9 @@ def main(url):
 
 if __name__ == '__main__':
 	urls = []
-	pagenums = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+	pagenums = []
+	for i in range(30):
+		pagenums.append(i)
 
 	for x in pagenums:
 		if x == 0:
